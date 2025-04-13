04dfa3d63ffd6205648eaf1dfc6b85f6c9aa581e
valeriabacelar <valeria_barrosb@hotmail.com>
2025-04-13 18:40:36 +0000

    printf\scanf-carta2

--- super_trunfo.c
+++ super_trunfo.c
@@ -11,11 +11,10 @@ int pontosturisticos;
 
 printf("Desafio Super Trunfo - Países\n");
 printf("Tema 1: Cadastro das Cartas\n");
-
-printf("Carta 1:\n");
+printf("Carta 1\n");
 printf("Digite o estado:");
 scanf("%c", &estado);
-printf("Digite o código da cidade:");
+printf("Digite o código da carta:");
 scanf("%s", &codigocarta);
 printf("Digite o Nome da cidade:");
 scanf("%s", &nomecidade);
@@ -28,4 +27,20 @@ scanf("%f", &pbi);
 printf("Digite a quantidade de Pontos Turísticos da cidade:");
 scanf("%d", &pontosturisticos);
 
+printf("\nCarta 2\n");
+printf("Digite o estado:");
+scanf("%c\n", &estado);
+printf("Digite o código da carta:");
+scanf("%s\n", &codigocarta);
+printf("Digite o Nome da cidade:");
+scanf("%s\n", &nomecidade);
+printf("Digite a População da cidade:");
+scanf("%d", &populacao);
+printf("Digite a Área da cidade:");
+scanf("%f\n", &area);
+printf("Digite o Produto Interno Bruto da cidade:");
+scanf("%f\n", &pbi);
+printf("Digite a quantidade de Pontos Turísticos da cidade:");
+scanf("%d", &pontosturisticos);
+return 0;
 }
